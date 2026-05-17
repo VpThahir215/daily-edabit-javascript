86ec2908e9d26f2a58deaf8a0148d581bed98dd8
VP THAHIR <vpthahir3062003@gmail.com>
2026-05-17 23:22:16 +0530

    Day4

---
+++ Day4/Day4.js
@@ -0,0 +1,127 @@
+
+let day=6;
+
+
+switch (day) {
+    case 1:
+        console.log("Sun");
+        
+        break;
+         case 2:
+        console.log("Mon");
+        
+        break;
+         case 3:
+        console.log("Tue");
+        
+        break;
+         case 4:
+        console.log("Wed");
+        
+        break;
+         case 5:
+        console.log("Thu");
+        
+        break;
+         case 6:
+        console.log("Fri");
+        
+        break;
+         case 7:
+        console.log("Sut");
+        
+        break;
+}
+  
+
+
+
+
+
+
+// // res="";
+
+// // for(i=1;i<=10;i++){
+// //   res+=i+" "; res = res + i 
+    
+
+// // }
+// // console.log(res)
+
+// // let a=10;
+// // let b=20;
+
+// // console.log(a+=b);
+// // console.log(a);
+
+// //Find sum of numbers from 1 to 5
+
+// // let res=0;
+// // let num=0;
+// // for(i=1;i<=5;i++){
+// //  res=i+res;
+// // }
+// // console.log(res);
+
+
+// //Count Even Number
+// // let res=0;
+// // for(i=2;i<=20;i++){
+// //     if(i%2==0){
+// //         res++;
+        
+       
+        
+// //     }
+        
+   
+// // }
+// // console.log(res);
+ 
+
+// // *
+// // * *
+// // * * *
+// // * * * *
+
+
+
+
+// * * * *
+// * * * 
+// * *
+// *
+// 
+
+// let res=""
+// for(let i=4;i>=0;i--){
+//     let res="";
+//     for(let j=0;j<i;j++){
+//     res+="* "
+      
+
+
+//     }
+//    console.log(res);   
+    
+// }
+
+
+
+
+ 
+  
+for (let i = 4; i >= 0; i--) {
+  let res = "";
+    if(i===4){
+res+=" " ;
+ }
+
+  for (let j = 1; j <= i; j++) {
+    res += "* ";
+   
+  }
+console.log(res);
+
+
+}
